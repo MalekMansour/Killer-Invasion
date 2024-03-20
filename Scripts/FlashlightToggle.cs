71e3101b5290314a8cab3005ba0b94daaa8ace9a
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-20 02:32:08 -0600

    Update FlashlightToggle.cs

--- Scripts/FlashlightToggle.cs
+++ Scripts/FlashlightToggle.cs
@@ -1,1 +1,23 @@
+using UnityEngine;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    public Light flashlight; 
+    private bool isFlashlightOn = false; 
+
+    void Start()
+    {
+        flashlight.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isFlashlightOn = !isFlashlightOn; 
+            flashlight.enabled = isFlashlightOn;
+        }
+    }
+}
 
