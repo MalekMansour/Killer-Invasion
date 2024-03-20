3c83a5345c212f6fb5c34ca5f8a5db64b73e0ceb
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-20 03:16:18 -0600

    Update GameManager.cs

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,1 +1,74 @@
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    public GameObject crosshair; 
+    public LayerMask interactableLayer; 
+    private bool isCursorLocked = true; 
+    private void Start()
+    {
+        LockCursor();
+    }
+
+    private void Update()
+    {
+        UpdateCrosshairPosition();
+
+        if (Input.GetMouseButtonDown(0) && isCursorLocked)
+        {
+            InteractWithObject();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleCursorLock();
+        }
+    }
+
+    void UpdateCrosshairPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10f; 
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        crosshair.transform.position = worldPosition;
+    }
+
+    void InteractWithObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, interactableLayer))
+        {
+            GameObject hitObject = hitInfo.collider.gameObject;
+            Debug.Log("Interacting with: " + hitObject.name);
+        }
+    }
+
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isCursorLocked = true;
+    }
+
+    void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isCursorLocked = false;
+    }
+
+    void ToggleCursorLock()
+    {
+        if (isCursorLocked)
+        {
+            UnlockCursor();
+        }
+        else
+        {
+            LockCursor();
+        }
+    }
+}
 
