4ae6ae347a94119594122d559cddec3845ad84ba
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-22 01:20:12 -0600

    Update InteractableObject.cs

--- Scripts/InteractableObject.cs
+++ Scripts/InteractableObject.cs
@@ -1,1 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string interactText = "Interact"; 
+    public TextMeshProUGUI interactTextUI; 
+    public float interactionRadius = 5f; 
+
+    private void Start()
+    {
+        interactTextUI.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Camera.main.transform.position);
+
+        if (distanceToPlayer <= interactionRadius)
+        {
+            interactTextUI.gameObject.SetActive(true);
+            interactTextUI.text = interactText;
+        }
+        else
+        {
+            interactTextUI.gameObject.SetActive(false);
+        }
+    }
+}
 
