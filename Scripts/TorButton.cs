419e7c2e88b3fb4cbf44ac8fc59e507aa4e85569
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-23 02:12:25 -0600

    Update TorButton.cs

--- Scripts/TorButton.cs
+++ Scripts/TorButton.cs
@@ -1,25 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TorButton : MonoBehaviour
 {
     public GameObject browserCanvas; 
 
-    private bool isBrowserOpen;
-
-    private void Start()
-    {
-        isBrowserOpen = false;
-        browserCanvas.SetActive(false); 
-    }
-
     public void ToggleBrowser()
     {
-        if (!isBrowserOpen) 
+        if (browserCanvas != null)
         {
-            isBrowserOpen = true;
             browserCanvas.SetActive(true); 
         }
     }
 }
-
