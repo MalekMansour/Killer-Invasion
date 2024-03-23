c5713e79ed6e98633ea68832d53afa9dc4f1aeee
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-23 02:02:13 -0600

    Update BrowserWindow.cs

--- Scripts/BrowserWindow.cs
+++ Scripts/BrowserWindow.cs
@@ -1,1 +1,29 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BrowserWindow : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler
+{
+    private RectTransform windowTransform;
+    private Vector2 offset;
+
+    private void Start()
+    {
+        windowTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        offset = eventData.position - (Vector2)windowTransform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 newPosition = eventData.position - offset;
+        windowTransform.position = newPosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+}
 
