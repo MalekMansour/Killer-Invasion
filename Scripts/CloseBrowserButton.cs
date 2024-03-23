2a5151f2a9597c945712592ea2e541d7ab0d9e2f
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-23 02:12:51 -0600

    Update CloseBrowserButton.cs

--- Scripts/CloseBrowserButton.cs
+++ Scripts/CloseBrowserButton.cs
@@ -1,1 +1,15 @@
+using UnityEngine;
+
+public class CloseBrowserButton : MonoBehaviour
+{
+    public GameObject browserCanvas; 
+
+    public void ToggleBrowser()
+    {
+        if (browserCanvas != null)
+        {
+            browserCanvas.SetActive(!browserCanvas.activeSelf); 
+        }
+    }
+}
 
