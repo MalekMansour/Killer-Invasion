ffed7a88834286419e7aa56dbe5bed7929d30803
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-25 13:21:37 -0600

    Update MaximizeButton.cs

--- Scripts/MaximizeButton.cs
+++ Scripts/MaximizeButton.cs
@@ -1,1 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaximizeButton : MonoBehaviour
+{
+    public Canvas targetCanvas; 
+    private bool isMaximized = false;
+    private Vector3 originalScale;
+    private Vector3 originalPosition;
+    private BrowserWindow browserWindow;
+
+    void Start()
+    {
+        originalScale = targetCanvas.transform.localScale;
+        originalPosition = targetCanvas.transform.localPosition;
+        browserWindow = targetCanvas.GetComponent<BrowserWindow>();
+    }
+
+    public void ToggleMaximize()
+    {
+        isMaximized = !isMaximized;
+
+        if (isMaximized)
+        {
+            targetCanvas.transform.localScale = new Vector3(0.165f, 0.285082f, 0.5334999f);
+            browserWindow.enabled = false;
+            targetCanvas.transform.localPosition = new Vector3(originalPosition.x, 11.3f, originalPosition.z);
+        }
+        else
+        {
+            targetCanvas.transform.localScale = originalScale;
+            browserWindow.enabled = true;
+            targetCanvas.transform.localPosition = originalPosition;
+        }
+    }
+}
 
