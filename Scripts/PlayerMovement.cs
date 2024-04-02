5ca5d350c893625c472c831981111759efc05090
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-04-02 01:58:27 -0600

    Update PlayerMovement.cs

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
-    public float sprintSpeed = 8f; 
-    private float currentSpeed; 
+    public float moveSpeed = 5f;
+    public float sprintSpeed = 8f;
+    private float currentSpeed;
+    private bool isMovementLocked = false;
+    private Stamina stamina; 
+
+    void Start()
+    {
+        stamina = GetComponent<Stamina>(); 
+    }
 
     void Update()
     {
-        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
+        if (!isMovementLocked)
+        {
+            currentSpeed = Input.GetKey(KeyCode.LeftShift) && stamina.currentStamina > 0 ? sprintSpeed : moveSpeed;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        transform.Translate(movementDirection * currentSpeed * Time.deltaTime);
+            transform.Translate(movementDirection * currentSpeed * Time.deltaTime);
+        }
+    }
+
+    public void SetMovementLocked(bool shouldLock)
+    {
+        isMovementLocked = shouldLock;
+
+        if (shouldLock)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
     }
 }
