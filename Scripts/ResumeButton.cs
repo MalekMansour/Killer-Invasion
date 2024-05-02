ce225f5bc8d2d39f6b5cec03eeade9d6bcc53661
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-05-01 19:31:39 -0600

    Update ResumeButton.cs

--- Scripts/ResumeButton.cs
+++ Scripts/ResumeButton.cs
@@ -1,1 +1,37 @@
+using UnityEngine;
+
+public class ResumeButton : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    public GameObject flashlight;
+
+    private bool isMouseLocked = true;
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; 
+        pauseCanvas.SetActive(false); 
+
+        if (flashlight != null)
+        {
+            flashlight.SetActive(true);
+        }
+
+        UpdateMouseLock();
+    }
+
+    void UpdateMouseLock()
+    {
+        if (isMouseLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked; 
+            Cursor.visible = false; 
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None; 
+            Cursor.visible = true; 
+        }
+    }
+}
 
