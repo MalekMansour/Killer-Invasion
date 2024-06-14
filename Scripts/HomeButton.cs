10bfd13874da4992be1e9a7b362c3ccaeeda9f60
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-06-13 18:14:03 -0600

    Update HomeButton.cs

--- Scripts/HomeButton.cs
+++ Scripts/HomeButton.cs
@@ -1,1 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeButton : MonoBehaviour
+{
+    public GameObject websitesParent; // The parent object containing all the website GameObjects
+    private GameObject currentActiveWebsite;
+
+    void Start()
+    {
+        // Add a listener to the button click event
+        Button homeButton = GetComponent<Button>();
+        if (homeButton != null)
+        {
+            homeButton.onClick.AddListener(ToggleOffCurrentWebsite);
+        }
+
+        // Initially, no website is active
+        currentActiveWebsite = null;
+    }
+
+    void Update()
+    {
+        // Update the current active website
+        foreach (Transform website in websitesParent.transform)
+        {
+            if (website.gameObject.activeSelf)
+            {
+                currentActiveWebsite = website.gameObject;
+                break;
+            }
+        }
+    }
+
+    void ToggleOffCurrentWebsite()
+    {
+        // Deactivate the currently active website
+        if (currentActiveWebsite != null)
+        {
+            currentActiveWebsite.SetActive(false);
+            currentActiveWebsite = null;
+        }
+    }
+}
 
