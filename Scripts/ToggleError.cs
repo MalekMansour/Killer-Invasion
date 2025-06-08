8a3d8570cd8e7be304c6733985901416596c7c87
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2025-06-08 02:12:29 -0600

    Update ToggleError.cs

--- Scripts/ToggleError.cs
+++ Scripts/ToggleError.cs
@@ -1,1 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwitchPage : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject currentPage; 
+    public GameObject targetPage;  
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (currentPage != null)
+            currentPage.SetActive(false);
+
+        if (targetPage != null)
+            targetPage.SetActive(true);
+    }
+}
 
