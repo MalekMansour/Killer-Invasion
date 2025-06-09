04346fdf5c3988867c8e6c099b7264dfadd2c1d6
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2025-06-09 17:21:07 -0600

    Update WebsiteOpener.cs

--- Scripts/WebsiteOpener.cs
+++ Scripts/WebsiteOpener.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 using System.Collections.Generic;
 
 public class WebsiteOpener : MonoBehaviour
 {
-    public GameObject websitesParent; 
-    public GameObject panel;
+    public GameObject websitesParent; // Parent object containing all website screens
+    public GameObject panel; // Button container
+    public TMP_InputField urlInputField; // Shared URL bar
+    public Image progressBarImage; // Blue progress fill image in URL bar
 
-    public Dictionary<GameObject, Sprite> websiteThumbnails = new Dictionary<GameObject, Sprite>(); 
-
+    public Dictionary<GameObject, Sprite> websiteThumbnails = new Dictionary<GameObject, Sprite>();
     private Dictionary<GameObject, GameObject> buttonToWebsiteMap = new Dictionary<GameObject, GameObject>();
     private List<GameObject> availableWebsites = new List<GameObject>();
+    private string baseURL = "https://www.thedarkwiki.com/redirect/main/";
 
     void Start()
     {
+        // Gather all available websites
         for (int i = 0; i < websitesParent.transform.childCount; i++)
         {
             GameObject website = websitesParent.transform.GetChild(i).gameObject;
@@ -26,6 +31,7 @@ public class WebsiteOpener : MonoBehaviour
             }
         }
 
+        // Set up button click events
         for (int i = 0; i < panel.transform.childCount; i++)
         {
             GameObject button = panel.transform.GetChild(i).gameObject;
@@ -46,34 +52,49 @@ public class WebsiteOpener : MonoBehaviour
     {
         if (buttonToWebsiteMap.ContainsKey(button))
         {
-            OpenWebsite(buttonToWebsiteMap[button]);
+            StartCoroutine(OpenWebsiteWithDelay(buttonToWebsiteMap[button]));
+        }
+        else if (availableWebsites.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableWebsites.Count);
+            GameObject assignedWebsite = availableWebsites[randomIndex];
+            availableWebsites.RemoveAt(randomIndex);
+
+            buttonToWebsiteMap[button] = assignedWebsite;
+            UpdateButtonThumbnail(button, assignedWebsite);
+            StartCoroutine(OpenWebsiteWithDelay(assignedWebsite));
         }
         else
         {
-            if (availableWebsites.Count > 0)
-            {
-                int randomIndex = Random.Range(0, availableWebsites.Count);
-                GameObject assignedWebsite = availableWebsites[randomIndex];
-                availableWebsites.RemoveAt(randomIndex);
-
-                buttonToWebsiteMap[button] = assignedWebsite;
-                OpenWebsite(assignedWebsite);
-                UpdateButtonThumbnail(button, assignedWebsite);
-            }
-            else
-            {
-                Debug.LogWarning("No more websites available to assign.");
-            }
+            Debug.LogWarning("No more websites available to assign.");
         }
     }
 
-    void OpenWebsite(GameObject website)
+    IEnumerator OpenWebsiteWithDelay(GameObject website)
     {
+        // Set a new random URL
+        string randomNumber = Random.Range(100000000, 999999999).ToString();
+        urlInputField.text = baseURL + randomNumber + ".onion";
+
+        // Reset progress bar
+        progressBarImage.fillAmount = 0f;
+        float duration = 7f;
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            progressBarImage.fillAmount = Mathf.Clamp01(timer / duration);
+            yield return null;
+        }
+
+        // Hide all websites
         for (int i = 0; i < websitesParent.transform.childCount; i++)
         {
             websitesParent.transform.GetChild(i).gameObject.SetActive(false);
         }
 
+        // Show selected one
         website.SetActive(true);
     }
 
